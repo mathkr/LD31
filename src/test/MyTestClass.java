a2f64ea984b73d2729780a13d9ae9c07fbcd7a09
Matthis Krause <matthiskrause@hotmail.com>
2014-12-05 20:15:21 +0100

    Modify MyTestClass

--- src/test/MyTestClass.java
+++ src/test/MyTestClass.java
@@ -4,4 +4,16 @@ package test;
  * Created by msk on 05.12.14.
  */
 public class MyTestClass {
+        private class Code {
+                String[] lots_of_code;
+        }
+
+        private class ClubMate {
+                int[] tasty_mate;
+        }
+
+        Code work(ClubMate[] lots_of_mate) {
+                System.out.println("Wurst");
+                return new Code();
+        }
 }
