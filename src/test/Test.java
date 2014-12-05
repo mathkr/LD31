47d4a6e28495a3bf96fdb1bd5e31bcedf028c2ed
Matthis Krause <matthiskrause@hotmail.com>
2014-12-05 15:48:47 +0100

    Initial setup

---
+++ src/test/Test.java
@@ -0,0 +1,51 @@
+package test;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+
+public class Test extends BasicGame
+{
+        private static int counter = 0;
+
+        public Test(String gamename)
+        {
+                super(gamename);
+        }
+
+        @Override
+        public void init(GameContainer gc) throws SlickException {
+                System.out.println("init: " + (counter++));
+        }
+
+        @Override
+        public void update(GameContainer gc, int i) throws SlickException {
+                System.out.println("update: " + (counter++));
+        }
+
+        @Override
+        public void render(GameContainer gc, Graphics g) throws SlickException
+        {
+                System.out.println("render: " + (counter++));
+                g.drawString("Howdy!", 40, 40);
+        }
+
+        public static void main(String[] args)
+        {
+                try
+                {
+                        AppGameContainer appgc;
+                        appgc = new AppGameContainer(new Test("Simple Slick Game"));
+                        appgc.setDisplayMode(640, 480, false);
+                        appgc.setTargetFrameRate(60);
+                        appgc.start();
+                }
+                catch (SlickException ex)
+                {
+                        Logger.getLogger(Test.class.getName()).log(Level.SEVERE, null, ex);
+                }
+        }
+}
