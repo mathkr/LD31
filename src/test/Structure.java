6a146e420f4d89eec5b84043041716b5c70140ab
Daniel <aircut@hotmail.de>
2014-12-06 14:55:02 +0100

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	src/test/MyTestClass.java

--- src/test/Structure.java
+++ src/test/Structure.java
@@ -13,7 +13,7 @@ public abstract class Structure {
 
         public abstract void update();
 
-        public boolean colidesWith(Structure other){
+        public boolean collidesWith(Structure other){
                 for(Vector2i thisPos : this.occupiedTiles)
                         for(Vector2i otherPos : other.occupiedTiles)
                                 if(this.position.x+thisPos.x == other.position.x+otherPos.x
