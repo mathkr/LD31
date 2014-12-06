11bd2ebbdbfeaa89705f4f1f9432b03f706129af
Daniel <aircut@hotmail.de>
2014-12-06 14:53:35 +0100

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	src/test/MyTestClass.java

--- src/test/Structure.java
+++ src/test/Structure.java
@@ -3,11 +3,24 @@ package test;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Structure {
+public abstract class Structure {
         public Vector2i position;
         public List<Vector2i> occupiedTiles;
 
         public Structure() {
                 occupiedTiles = new ArrayList<Vector2i>();
         }
+
+        public abstract void update();
+
+        public boolean colidesWith(Structure other){
+                for(Vector2i thisPos : this.occupiedTiles)
+                        for(Vector2i otherPos : other.occupiedTiles)
+                                if(this.position.x+thisPos.x == other.position.x+otherPos.x
+                                        && this.position.y+thisPos.y == other.position.y+otherPos.y)
+                                        return true;
+                return false;
+        }
+
+        public abstract ResourceTable getCost();
 }
