9b47d1441d6fe58289eec8aa46181a6e5dc857fe
Daniel <aircut@hotmail.de>
2014-12-06 14:53:56 +0100

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	src/test/MyTestClass.java

---
+++ src/test/ResourceTable.java
@@ -0,0 +1,12 @@
+package test;
+
+/**
+ * Created by Daniel on 06.12.2014.
+ */
+public class ResourceTable {
+
+    public int copper;
+    public int silver;
+    public int glass;
+    public int silicon;
+}
